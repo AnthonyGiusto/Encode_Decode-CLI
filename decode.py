62e1c27e8a28f8b02acf0deff899769392df751d
Anthony Giusto <giustoanthony@gmail.com>
2020-01-11 10:24:27 +0100

    Code Cleanup
    Cleaned up code, with better formatting, added comments, and reduced unneeded or redundant code.

--- decode.py
+++ decode.py
@@ -1,26 +1,29 @@
-class Decode:
+def decode(index):
+    """Takes a user input value and decodes it with
+    a predefined index"""
 
-	def __init__(self,month):
-		"""initializes the encode attributes."""
-		self.month = month
+    # creates known lists of indexes for decoding
+    nullcode = {'i': 1, 'c': 2, 'e': 3, 'd': 4, 'r': 5, 'a': 6,
+                'g': 7, 'o': 8, 'n': 9, 's': 0}
+    code_one = {'n': 1, 'e': 2, 'a': 3, 'r': 4, 'd': 5,
+                'g': 6, 's': 7, 'i': 8, 'o': 9, 'c': 0}
+    code_two = {'c': 1, 's': 2, 'g': 3, 'r': 4, 'd': 5, 'i': 6,
+                'o': 7, 'a': 8, 'e': 9, 'n': 0}
+    code_three = {'a': 1, 'e': 2, 'r': 3, 'n': 4, 's': 5, 'i': 6,
+                  'o': 7, 'g': 8, 'c': 9, 'd': 0}
+    code_four = {'d': 1, 'r': 2, 'a': 3, 's': 4, 'g': 5,
+                 'o': 6, 'c': 7, 'i': 8, 'e': 9, 'n': 0}
 
-	def decode(self):
-		"""Takes a user input value and decodes it with a predefined month index"""
+    # Assigns each indexed code to the codelist list object.
+    codelist = [nullcode, code_one, code_two, code_three, code_four]
 
-		#creates known lists of indexes for decoding
-		nullcode = {'i':1,'c':2,'e':3,'d':4,'r':5,'a':6,'g':7,'o':8,'n':9,'s':0}
-		code_one = {'n':1,'e':2,'a':3,'r':4,'d':5,'g':6,'s':7,'i':8,'o':9,'c':0}
-		code_two = {'c':1,'s':2,'g':3,'r':4,'d':5,'i':6,'o':7,'a':8,'e':9,'n':0}
-		code_three = {'a':1,'e':2,'r':3,'n':4,'s':5,'i':6,'o':7,'g':8,'c':9,'d':0}
-		code_four = {'d':1,'r':2,'a':3,'s':4,'g':5,'o':6,'c':7,'i':8,'e':9,'n':0}
-		codelist = [nullcode,code_one, code_two, code_three, code_four]
+    # Prompts for the code-string that is to be decoded into a number.
+    code = input("enter the code: ")
+    letterlist = []
 
-		#User-input for the code-string to be decoded into a number
-		code = input("\nenter the code: ")
-		letterlist = []
-		codeindex = codelist[self.month]
-		for letter in code:
-			letterlist.append(letter)
-		for letter in letterlist:
-			print (codeindex[letter], end = "")
-		return letterlist
+    # Assigns the user-defined index to the corrilated code list.
+    codeindex = codelist[index]
+    for letter in code:
+        letterlist.append(letter)
+    for letter in letterlist:
+        print(codeindex[letter], end="")
--- main.py
+++ main.py
@@ -1,43 +1,49 @@
-from encode import Encode
-from decode import Decode
+from encode import encode
+from decode import decode
 
 
 def main():
+    """
+    Runs the Encoder-Decoder application from the command line.  This asks the
+    the user for an input and a code index and either encodes or decodes the
+    input, based on the user option.
+    """
 
-	option = input("press 'E' for encode or 'D' for decode: ").lower()
-	if option == 'e':
+    option = input("press 'E' for encode or 'D' for decode: ").lower()
+    if option == 'e':
 
-		month = input("Please enter your index: ")
-		month = int(month)
-		while month < 0 or month > 12:
-				print("index is out of range.")
-				month = input("Please re-enter your index: ")
-				month = int(month)
-				continue
-		encodednumber = Encode(month)
-		encodedresponse = encodednumber.encode()
+        index = input("Please enter your index: ")
+        index = int(index)
+        while index < 0 or index > 12:
+            print("index is out of range.")
+            index = input("Please re-enter your index: ")
+            index = int(index)
+        encodedresponse = encode(index)
 
-	elif option == 'd':
-		month = input("Please enter your index: ")
-		month = int(month)
-		while month < 0 or month > 12:
-				print("index is out of range.")
-				month = input("Please re-enter your index: ")
-				month = int(month)
-				continue
-		decodednumber=Decode(month)
-		decodedresponse = decodednumber.decode()
-	else:
-		print ("Invalid response")
+    elif option == 'd':
+        index = input("Please enter your index: ")
+        index = int(index)
+        while index < 0 or index > 12:
+            print("index is out of range.")
+            index = input("Please re-enter your index: ")
+            index = int(index)
+        decodednumber = decode(index)
+    else:
+        print("Invalid response")
+
 
 main()
+
+# prompts the user for whether they want to rerun the program or quit.
 while True:
-	response = input("\nWould you like to continue?\nPress 'Q' to quit or 'Y' to continue: ").lower()
-	if response == 'q':
-		break
-	elif response == 'y':
-		main()
-		continue
-	else:
-		print ("Invalid response")
-		continue
+    responsestr = "\nWould you like to continue?\nPress 'Q' to quit or 'Y' "\
+        "to continue: "
+    response = input(responsestr).lower()
+    if response == 'q':
+        break
+    elif response == 'y':
+        main()
+        continue
+    else:
+        print("Invalid response")
+        continue
