c1e895ff727aca1b49108c99dfabbbb88c07c4cc
Anthony Giusto <giustoanthony@gmail.com>
2020-01-11 10:29:53 +0100

    rename files
    renamed files in preparation to merge with the GUI repository

---
+++ main_CLI.py
@@ -0,0 +1,49 @@
+from encode_CLI import encode
+from decode_CLI import decode
+
+
+def main():
+    """
+    Runs the Encoder-Decoder application from the command line.  This asks the
+    the user for an input and a code index and either encodes or decodes the
+    input, based on the user option.
+    """
+
+    option = input("press 'E' for encode or 'D' for decode: ").lower()
+    if option == 'e':
+
+        index = input("Please enter your index: ")
+        index = int(index)
+        while index < 0 or index > 12:
+            print("index is out of range.")
+            index = input("Please re-enter your index: ")
+            index = int(index)
+        encodedresponse = encode(index)
+
+    elif option == 'd':
+        index = input("Please enter your index: ")
+        index = int(index)
+        while index < 0 or index > 12:
+            print("index is out of range.")
+            index = input("Please re-enter your index: ")
+            index = int(index)
+        decodednumber = decode(index)
+    else:
+        print("Invalid response")
+
+
+main()
+
+# prompts the user for whether they want to rerun the program or quit.
+while True:
+    responsestr = "\nWould you like to continue?\nPress 'Q' to quit or 'Y' "\
+        "to continue: "
+    response = input(responsestr).lower()
+    if response == 'q':
+        break
+    elif response == 'y':
+        main()
+        continue
+    else:
+        print("Invalid response")
+        continue
